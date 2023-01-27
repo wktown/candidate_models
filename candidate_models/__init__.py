7eaab62e2979b3f9d76ee6ce9be9cd0070f6f9bd
wktown <112629266+wktown@users.noreply.github.com>
2023-01-27 15:29:44 -0500

    Update __init__.py
    import get_stimulus_set directly from brainio.fetch (no brainscore)

--- candidate_models/__init__.py
+++ candidate_models/__init__.py
@@ -1,10 +1,10 @@
 import logging
 
-import brainscore
+from brainio.fetch import get_stimulus_set
 from candidate_models.model_commitments import brain_translated_pool
 
 _logger = logging.getLogger(__name__)
 
 def get_activations(model, layers, stimulus_set):
-    stimuli = brainscore.get_stimulus_set(stimulus_set)
+    stimuli = get_stimulus_set(stimulus_set)
     return model(stimuli, layers=layers)
