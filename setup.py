c03cc8801fe1d6786a479935ecd5eebe4f4ab31e
wktown <112629266+wktown@users.noreply.github.com>
2023-01-27 14:56:26 -0500

    Update setup.py
    removing brainscore (and model tools) dependence for scikit-learn nshot classification

--- setup.py
+++ setup.py
@@ -8,8 +8,6 @@ with open('README.md') as readme_file:
 
 requirements = [
     "netcdf4",
-    "brain-score @ git+https://github.com/brain-score/brain-score",
-    "model-tools @ git+https://github.com/brain-score/model-tools",
     "numpy",
     "result_caching @ git+https://github.com/brain-score/result_caching",
     "networkx==1.11",
